7c0f3d58b5af21707ad2e96f2400a125729b6486
Kurachi <naist1020@gmail.com>
2019-05-17 14:33:06 +0900

    コメントを挿入しました

--- scraping_ireko.py
+++ scraping_ireko.py
@@ -8,21 +8,16 @@ import re
 url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/utility_functions-blind-clones/utility_functions-blind-clones-0.30-classes-withsource.html"
 res = req.urlopen(url)
 
-#
-# .textで取得する方法
-#
-# soup = BeautifulSoup(html) と書いたらWarningが出るため下記コードにする
 # HTMLを解析
 soup = BeautifulSoup(res, "html.parser")
 
+# 任意のデータを抽出 --- (※1)
 f = open('Scraping_NiCad.txt','w')
-
 
 data = defaultdict(list)
 product_div = soup.find('body')
 
-# code = soup.find('pre').extract()
-
+# クローンクラス<h3>の配列を作成し,類似コードが存在するファイルパス<td>を要素として配列に格納する処理
 for product in product_div.find_all(['h3', 'td']):
 	if product.name == 'h3':
 		key = product.text.replace('\n','').replace('\r','')
@@ -33,7 +28,6 @@ for product in product_div.find_all(['h3', 'td']):
 			product.find('pre').decompose()
 		except AttributeError:
 			pass
-		# path = product.text.replace('\n','').replace('\r','')
 		path = product.text
 		path = product.text.replace('\n','').replace('\r','')
 		edit_path = re.sub(r"Lines.*?systems/", "", path)
@@ -43,21 +37,7 @@ for product in product_div.find_all(['h3', 'td']):
 		f.write(edit_path)
 		f.write("\n")
 print(data)
-# src = soup.select("table > tr > td")
-# # print(type(src))
-# for i in src:
-#     i.find('pre').decompose()
-#     f.write(i.text.replace('\n',''))
-#     f.write("\n")
-#     print(i.text.replace('\n',''))
 
-# cclass = soup.select("body > h3")
-
-# for t in cclass:
-#     # if ".java" in s.string:
-#         f.write(t.string)
-#         f.write("\n")
-#         print(t.string.replace('\n',''))
 
     
 
