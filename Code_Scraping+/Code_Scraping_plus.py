3d46ff4acd74b72b142fda03d52d471109115975
Kurachi <naist1020@gmail.com>
2019-06-16 21:44:21 +0900

    add edit

--- Code_Scraping+/Code_Scraping_plus.py
+++ Code_Scraping+/Code_Scraping_plus.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 import urllib.request as req
 import re
 import csv
+import collections
 
 # テスト用のHTML
 #url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/utility_functions-blind-clones/utility_functions-blind-clones-0.30-classes-withsource.html"
@@ -11,7 +12,7 @@ import csv
 #url = "file:///C:/Users/ryosuke-ku/Desktop/apache_2_functions-blind-clones-0.30-classes-withsource.html"
 #url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/android_platform_projects_functions-blind-clones/android_platform_projects_functions-blind-clones-0.30-classes-withsource.html"
 #url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/apache_activemq_projects_functions-blind-clones/apache_activemq_projects_functions-blind-clones-0.30-classes-withsource.html"
-#url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/maven_functions-blind-clones/maven_functions-blind-clones-0.30-classes-withsource.html"
+url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/maven_functions-blind-clones/maven_functions-blind-clones-0.30-classes-withsource.html"
 #url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/apache_ant_maven_functions-blind-clones/apache_ant_maven_functions-blind-clones-0.30-classes-withsource.html"
 #url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/Server_Projects_functions-blind-clones/Server_Projects_functions-blind-clones-0.30-classes-withsource.html"
 #url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/RxJava_functions-blind-clones/RxJava_functions-blind-clones-0.30-classes-withsource.html"
@@ -27,7 +28,7 @@ import csv
 #url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/cassandra_functions-blind-clones/cassandra_functions-blind-clones-0.30-classes-withsource.html"
 #url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/httpcomponents-client_functions-blind-clones/httpcomponents-client_functions-blind-clones-0.30-classes-withsource.html"
 #url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/hadoop_functions-blind-clones/hadoop_functions-blind-clones-0.30-classes-withsource.html"
-url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/ant_functions-clones/ant_functions-clones-0.00-classes-withsource.html"
+#url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/ant_functions-clones/ant_functions-clones-0.00-classes-withsource.html"
 #url = "file:///C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/ant_functions-blind-clones/ant_functions-blind-clones-0.00-classes-withsource.html"
 res = req.urlopen(url)
 
@@ -91,7 +92,7 @@ TPath = [Tline.replace('\n', '') for Tline in TestPath]
 
 alltest=0
 for p in TPath:
-	if re.match('ant.*?/', p):
+	if re.match('maven.*?/', p):
 		alltest += 1
 
 print(alltest)
@@ -112,11 +113,14 @@ totalpairs = 0
 totalfragments = 0
 notest = 0
 reusetest = []
+list_nt = []
+list_at = []
+list_pt = []
 
 for i in data:
 	f.write(i)
 	f.write("\n") 
-#	print(i)
+	print(i)
 	fragments = len(data[i])
 	# print(len(data[i]))
 	count = 0
@@ -136,22 +140,24 @@ for i in data:
 			f.write("\n")
 			reusetest.append(path)
 			count += 1
-			# print(path)
+			print(path)
 
 	# print(count)
 	judgment = fragments - count
 	if judgment == fragments:
 		# print("テストコードが見つかりませんでした")
 		nt += 1
+		list_nt.append(i[-4:].replace(" ",""))
 	elif judgment == 0:
 		#print("すべてのコードフラグメントがテストコードを持っています")
 		at += 1
+		list_at.append(i[-4:].replace(" ",""))
 	elif 0 < judgment < fragments :
 		#print("クローンペアのうち少なくとも一つのコードフラグメントはテストコードを持っています")
 		#print("他のテストを再利用できそうなフラグメントの数："+ str(judgment))
 		notest += judgment
 		pt +=1
-
+		list_pt.append(i[-4:].replace(" ",""))
 
 #	print(len(data[i])*(len(data[i])-1)/2)
 	totalpairs += len(data[i])*(len(data[i])-1)/2
@@ -160,6 +166,17 @@ for i in data:
 print(len(reusetest))
 reusetestpath = list(set(reusetest))
 print(len(reusetestpath))
+
+parcent = [k[-4:].replace(" ","") for k in data]
+# print(parcent)
+
+# editparcent = [j.replace(" ","") for j in parcent]
+print(parcent)
+
+c = collections.Counter(parcent)
+print(c)
+print(len(c))
+print(len(parcent))
 
 # csvFile.close()
 print("\n")
@@ -189,6 +206,25 @@ r.write("\n")
 r.write(str(at) + " すべてのコードフラグメントがテストコードを持っています ")
 r.write("\n")
 r.write("-----------------------------------------------------------------------------------")
+
+
+c_nt = collections.Counter(list_nt)
+print(c_nt)
+print(len(list_nt))
+
+c_pt = collections.Counter(list_pt)
+print(c_pt)
+print(len(list_pt))
+
+c_at = collections.Counter(list_at)
+print(c_at)
+print(len(list_at))
+
+
+
+print(len(list_nt)+len(list_at)+len(list_pt))
+
+
 
 
 cnt = 0
